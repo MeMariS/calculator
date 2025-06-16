ff8a27abed31194cb89078a8d236dfddb17010e8
Maria Sidorova <mariasidorova@MacBook-Air-Maria.local>
2025-06-16 20:18:35 +0300

    add method this

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
+const advancedCalculator = {
+  a: 0,
+  b: 0,
+
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  add() {
+    return this.a + this.b;
+  },
+
+  sub() {
+    return this.a - this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  div() {
+    if (this.b === 0) {
+      return "Ошибка деления на ноль";
+    }
+    return this.a / this.b;
+  },
+
+  reset() {
+    this.a = null;
+    this.b = null;
+  },
+};
+
 function calculate() {
   let a = Number(document.getElementById("a").value);
   let b = Number(document.getElementById("b").value);
   let operation = document.querySelector(
     'input[name="operation"]:checked'
   ).value;
-  let result;
 
   if (isNaN(a) || isNaN(b)) {
-    result = "Пожалуйста, введите оба числа.";
-  } else {
-    if (operation === "+") {
-      result = a + b;
-    } else if (operation === "-") {
-      result = a - b;
-    } else if (operation === "*") {
-      result = a * b;
-    } else if (operation === "/") {
-      result = b !== 0 ? a / b : "Ошибка: деление на ноль";
-    }
+    document.getElementById("result").textContent =
+      "Пожалуйста, введите оба числа.";
+    return;
+  }
+
+  advancedCalculator.read(a, b);
+
+  let result;
+
+  if (operation === "+") {
+    result = advancedCalculator.add();
+  } else if (operation === "-") {
+    result = advancedCalculator.sub();
+  } else if (operation === "*") {
+    result = advancedCalculator.mul();
+  } else if (operation === "/") {
+    result = advancedCalculator.div();
   }
 
   document.getElementById("result").textContent = "Результат: " + result;
